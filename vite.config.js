4f6bfca06734decd472b1f27272b4c562f9cb0ed
mestupinanm <m.estupinanm@uniandes.edu.co>
2026-04-22 17:52:28 -0500

    versiones tablet

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    legacy({
+      targets: ['chrome >= 30', 'android >= 4.4', 'safari >= 8', 'ios >= 8'],
+      additionalLegacyPolyfills: ['whatwg-fetch', 'core-js/proposals/global-this']
+    })
+  ]
 });
